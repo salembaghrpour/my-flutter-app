0fd2a6319c96265e1f1feefdb0c02d6d2c5dba8d
Ali Bagherpour <poraryaa@gmail.com>
2026-07-15 23:32:21 +0330

    Fix flutter plugin version in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     // این خط برای فایربیس اضافه شد
-    id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 include(":app")
