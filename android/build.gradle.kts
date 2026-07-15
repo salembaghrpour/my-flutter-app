97d0d95eff07cc52cfcaa0d8163486f36001934c
Ali Bagherpour <poraryaa@gmail.com>
2026-07-16 00:06:40 +0330

    توضیحات مربوط به تغییرات جدید

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,10 +12,21 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// این بخش برای رفع مشکل namespace پکیج‌های قدیمی اضافه شد
+subprojects {
+    afterEvaluate {
+        val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        if (android != null && android.namespace == null) {
+            android.namespace = project.group.toString()
+        }
+    }
 }
